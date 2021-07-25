19c82411a2041570cbc14ea154dac12fdeaaf655
Louis <mycodelearningemail@protonmail.com>
2021-07-25 16:18:39 +0900

    new codewar

--- 05-07-2022/main.js
+++ 05-07-2022/main.js
@@ -5,9 +5,9 @@
 
 //Solution:
 
-function greet(name){
-    return `Hello, ${name} how are you doing today?`
-  }
+// function greet(name){
+//     return `Hello, ${name} how are you doing today?`
+//   }
 
 
 //Sample Tests:
@@ -22,3 +22,31 @@ function greet(name){
 //   })
 // })
 
+
+
+// Problem:
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+
+//Solution:
+function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
+    hoopCount(1)
+
+
+
+//Tests
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(hoopCount(3),"Keep at it until you get it" ) 
+//   Test.assertEquals(hoopCount(11),"Great, now move on to tricks" )
+//     });
+//   });
+  
