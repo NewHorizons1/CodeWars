516bd20c84910b496e9530bcd64af4828d2ddb4e
Louis <mycodelearningemail@protonmail.com>
2022-08-17 21:32:01 +0900

    New codewars

--- 08-16-2022/main.js
+++ 08-16-2022/main.js
@@ -15,3 +15,20 @@ function numberToString(num) {
 }
 
 //The above solution returns the parameter num to a string.
+
+
+
+//Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+
+//Solution:
+
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.replace(/T/g, "U")
+}
