38a8e1e7a1f3c292a9d243438d51c46b7bb7046c
Louis <mycodelearningemail@protonmail.com>
2022-08-25 22:31:14 +0900

    New codewar

---
+++ 08-25-2022/main.js
@@ -0,0 +1,26 @@
+//Problem:
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455
+
+//Solution:
+
+function sumTwoSmallestNumbers(numbers) {
+    let first = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(first), 1)
+    let second = Math.min(...numbers)
+    return first + second
+}
+
+//Problem:
+// Write a function to split a string and convert it into an array of words.
+
+
+//Solution:
+
+function stringToArray(string) {
+    let string = string.split()
+
+}
